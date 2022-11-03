ed2697eabdc4a125756dad313c5a4b2e5489b3ab
AhsenZafar <100182384+AhsenZafar@users.noreply.github.com>
2022-11-02 18:50:50 -0600

    begin js code
    begin script to render menu item data to site via js

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 import { menuArray  } from './data.js'
-console.log(menuArray)
+const itemContainer = document.getElementById('menu')
 
-document.getElementById('header').innerHTML = `<h2>Best Burgers and Poooza in Town</h2>`
+
+function getMenu(){
+    let menu =''
+    menuArray.forEach(function(item){
+        menu +=
+        
+        `<div class ='item'>
+            <div class = 'left-container'>
+                <p class ='emoji'>{${item.emoji}}</p>
+                <div class ='description'>
+                    <h3>Pizza</h3>
+                    <p>ingredients</p>
+                    <p>price</p>
+                </div>
+            </div>
+            <div class ='right-container'>
+                <button class = 'add-item' data-itemNo="0">+</button>
+            </div>
+            </div>
+            
+                
+
+    
+        </div>`
+
+
+   
+    })
+    itemContainer.innerHTML = menu
+}
+getMenu()
